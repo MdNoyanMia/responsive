9263d61b991c875b7c3367d3cc9c88485ffbde1b
noyanraz <noyanraz3@gmail.com>
2025-05-08 08:10:12 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$("#hide").click(function(){
+    $(".box").hide(1000)
+})
+
+$("#show").click(function(){
+    $(".box").show(1000)
+})
+
+$("#both").click(function(){
+    $(".box").toggle(1000)
+})
+
+$("#slideup").click(function(){
+    $(".box").slideUp(1000)
+})
+
+$("#slidedown").click(function(){
+    $(".box").slideDown(1000)
+})
+$("#slideUpDown").click(function(){
+    $(".box").slideToggle(1000)
+})
+
